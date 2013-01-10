1f6e76c8ce3722fe62e0cd6c423c9dc4bb9d790e
Armael <armael.gueneau@ens-lyon.fr>
2013-01-10 01:46:22 +0100

    Oops (maintenant ça compile)

--- event.ml
+++ event.ml
@@ -3,7 +3,7 @@ type ('a,'b,'c) event_handler =
     button_handler : bool -> 'b;
     pos_handler : (int*int)-> 'c }
 
-let create_handler ()= 
+let create_handler () =
   { keypress_handler = (fun c -> ());
     button_handler = (fun b -> ());
     pos_handler = (fun (i, j) -> ()) }
@@ -14,17 +14,17 @@ let set_pos_handler fkh h = {h with pos_handler = fkh}
 let mouse_memory = ref false
 
 let handle_event event_hand =
+  let open Graphics in
   let status = Graphics.wait_next_event [Graphics.Key_pressed;
 					 Graphics.Button_down;
 					 Graphics.Button_up;
 					 Graphics.Poll] in 
-  let aux b f a=
+  let aux b f a =
     match b with 
     | true -> Some (f a)
     | false -> None in
   
   let res = 
-    open Graphics in
   (aux status.keypressed event_hand.keypress_handler status.key,
    aux (!mouse_memory = status.button) event_hand.button_handler status.button,
    Some (event_hand.pos_handler (status.mouse_x, status.mouse_y))) in
