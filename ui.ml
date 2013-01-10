e97fef2a5871effaac4bf20abef4a201fbc75a62
Diego Nava Saucedo <diego.nava_saucedo@ens-lyon.fr>
2013-01-10 18:13:03 +0100

    ajout du slide dans les actions

--- ui.ml
+++ ui.ml
@@ -1,15 +1,24 @@
-type t = Keypress of char | Button_up | Button_down
+type t = Keypress of char | Button_up | Button_down | Slide of Vector.t * Vector.t
 type pos = Pos of int * int
 type status = ((t * pos) list)
 
 let mouse_memory = ref false
+let mouse_pos_memory = ref (Vector.create ())
+
 
 let get_status () =
   let button_get button_stat pos l =
     let aux button_stat pos l =
       match button_stat <> !mouse_memory, button_stat with
-      |true, true -> (Button_up, pos)::l
-      |true, false -> (Button_down, pos)::l
+      |true, true -> mouse_pos_memory := {Vector.x = float_of_int (fst pos); Vector.y = float_of_int (snd pos)};
+      							  (Button_up, Pos (fst pos,snd pos))::l
+      |true, false -> 
+      									if ((!mouse_pos_memory).Vector.x <> (float_of_int (fst pos)) ||  (!mouse_pos_memory).Vector.y <> (float_of_int (snd pos)))
+      									then 
+      									((Slide (!mouse_pos_memory,{Vector.x = float_of_int (fst pos); Vector.y = float_of_int (snd pos)}), 
+      									Pos (fst pos,snd pos)))::
+      									(Button_down, Pos (fst pos,snd pos))::l
+      									else(Button_down, Pos (fst pos,snd pos))::l
       |false, _ -> l
     in
     let res = aux button_stat pos l in
@@ -19,7 +28,7 @@ let get_status () =
 
   let key_get key_stat key_char pos l=
     match key_stat with
-    |true -> ((Keypress (key_char())), pos)::l
+    |true -> ((Keypress (key_char())), Pos (fst pos,snd pos))::l
     |false -> l
   in
 
@@ -33,9 +42,9 @@ let get_status () =
 	   				     Graphics.Poll] in 
       let open Graphics in
       (button_get statbe.button 
-	 (Pos (statbe.mouse_x, statbe.mouse_y))
+	 ((statbe.mouse_x, statbe.mouse_y))
 	 (key_get (key_pressed()) read_key 
-	    (Pos ((fst (mouse_pos())), snd (mouse_pos())))
+	    (((fst (mouse_pos())), snd (mouse_pos())))
 	    (get_event (key_pressed()) (i-1)))) 
   in
   get_event true 5
--- ui.mli
+++ ui.mli
@@ -1,4 +1,4 @@
-type t = Keypress of char | Button_up | Button_down
+type t = Keypress of char | Button_up | Button_down | Slide of Vector.t * Vector.t
 type pos = Pos of int * int
 type status = ((t * pos) list)
 
